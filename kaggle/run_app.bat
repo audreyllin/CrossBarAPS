2aa46b88afd22e4b0690d6e3651cdb047b4e0b03
audreyllin <audrey.lin@crossbar-inc.com>
2025-07-10 14:48:31 -0700

    Delete kaggle/run_app.bat

--- kaggle/run_app.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set OPENAI_API_KEY=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
-set ADMIN_USER=audrey
-set ADMIN_PASS=cR0Ss@):
-python app.py
-pause
